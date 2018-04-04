d56adee35360b715725ade85bdea5308b1086711
Robert Gonzales <robertgcreative@gmail.com>
2018-04-04 17:33:29 -0400

    rough overlay styles

--- src/App.js
+++ src/App.js
@@ -1,77 +1,79 @@
-import React from "react";
-import gql from "graphql-tag";
-import { compose, graphql } from "react-apollo";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import styled from "styled-components";
-import { IntroOverlay, Map, Place, SideBar } from "./components";
-import Dock from "react-dock";
+import React from "react"
+import gql from "graphql-tag"
+import { compose, graphql } from "react-apollo"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import styled from "styled-components"
+import { IntroOverlay, Map, Place, SideBar } from "./components"
+import Dock from "react-dock"
 
 const Container = styled.div`
-	width: 100%;
-	height: 100%;
-	position: relative;
-`;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`
 const Footer = styled.div`
-	text-align: center;
-	width: 100%;
-	height: 40px;
-	z-index: 10;
-	position: absolute;
-	bottom: 0px;
-	left: 0px;
-`;
+  text-align: center;
+  width: 100%;
+  height: 40px;
+  z-index: 10;
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+`
 
 class App extends React.Component {
-	state = {
-		showIntro: false
-	};
+  state = {
+    showIntro: false,
+  }
 
-	componentWillMount() {
-		if (window.localStorage && !window.localStorage.getItem("visited")) {
-			this.setState({ showIntro: true });
-		}
-	}
+  componentWillMount() {
+    if (window.localStorage && !window.localStorage.getItem("visited")) {
+      this.setState({ showIntro: true })
+    }
+  }
 
-	handleEnterSite = () => {
-		this.setState({ showIntro: false });
-		if (window.localStorage) {
-			window.localStorage.setItem("visited", true);
-		}
-	};
+  handleEnterSite = () => {
+    this.setState({ showIntro: false })
+    if (window.localStorage) {
+      window.localStorage.setItem("visited", true)
+    }
+  }
 
-	render() {
-		return (
-			<Router>
-				<div>
-					<Route
-						path="/p/:placeId"
-						children={({ match, history }) => (
-							<Container>
-								<Map
-									activePlaceId={match && match.params.placeId}
-									onSelectPlace={placeId => history.push(`/p/${placeId}`)}
-								/>
-								<Dock
-									position="right"
-									isVisible={!!match}
-									onVisibleChange={() => !!match && history.push("/")}>
-									{match && <Place id={match.params.placeId} />}
-								</Dock>
-								<Footer>
-									Built by{" "}
-									<a href="https://www.pathwright.com">The Pathwright Crew</a>{" "}
-									for <a href="https://goodfour.us">Good Four</a> 2018
-								</Footer>
-							</Container>
-						)}
-					/>
-					{this.state.showIntro && (
-						<IntroOverlay onClose={this.handleEnterSite} />
-					)}
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route
+            path="/p/:placeId"
+            children={({ match, history }) => (
+              <Container>
+                <Map
+                  activePlaceId={match && match.params.placeId}
+                  onSelectPlace={placeId => history.push(`/p/${placeId}`)}
+                />
+                <Dock
+                  position="right"
+                  isVisible={!!match}
+                  onVisibleChange={() => !!match && history.push("/")}>
+                  {match && <Place id={match.params.placeId} />}
+                </Dock>
+                <Footer>
+                  Built by{" "}
+                  <a href="https://www.pathwright.com">
+                    The Pathwright Crew
+                  </a>{" "}
+                  for <a href="https://goodfour.us">Good Four</a> 2018
+                </Footer>
+              </Container>
+            )}
+          />
+          {this.state.showIntro && (
+            <IntroOverlay onClose={this.handleEnterSite} />
+          )}
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
