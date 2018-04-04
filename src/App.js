1f6d905230fa11c13436cacad868eb1ea672b074
Robert Gonzales <robertgcreative@gmail.com>
2018-04-04 17:19:01 -0400

    add intro overlay, fix misc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from "react"
 import gql from "graphql-tag"
 import { compose, graphql } from "react-apollo"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import { Map, Place, SideBar } from "./components"
+import { IntroOverlay, Map, Place, SideBar } from "./components"
 import Dock from "react-dock"
 
 class App extends React.Component {
+  state = {
+    showIntro: false,
+  }
+
+  componentWillMount() {
+    if (window.localStorage && !window.localStorage.getItem("visited")) {
+      this.setState({ showIntro: true })
+    }
+  }
+
+  handleEnterSite = () => {
+    this.setState({ showIntro: false })
+    if (window.localStorage) {
+      window.localStorage.setItem("visited", true)
+    }
+  }
+
   render() {
     return (
       <Router>
@@ -27,6 +44,9 @@ class App extends React.Component {
               </React.Fragment>
             )}
           />
+          {this.state.showIntro && (
+            <IntroOverlay onClose={this.handleEnterSite} />
+          )}
         </div>
       </Router>
     )
